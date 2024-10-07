4dd408eb354665c293627bb2c70d395ae37bfbd8
FYMG <svinpetr18@gmail.com>
2024-10-07 19:58:32 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import * as fs from "node:fs/promises";
+import throwErrorWhenFileNeverExist from "../helpers/throwErrorWhenFileNeverExist.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRemove.txt')
+
+  try {
+    await throwErrorWhenFileNeverExist(filePath)
+    await fs.rm(filePath, {recursive: true})
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await remove();
