57cf9bd70b772d85022fe5d499fbc9e1ecccef19
FYMG <svinpetr18@gmail.com>
2024-10-07 17:51:41 +0300

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const result = process.argv
+    .slice(2)
+    .reduce((acc, curr, index, array) => {
+      if (index % 2 === 0) {
+        acc.push(`${curr.slice(2)} is ${array[index + 1]}`);
+      }
+
+      return acc;
+    }, [])
+    .join(', ');
+
+  console.log(result);
 };
 
 parseArgs();
