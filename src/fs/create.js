27b27ebfe435bdd178f6005423e97cadc90b6b8e
FYMG <svinpetr18@gmail.com>
2024-10-07 19:05:34 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import * as fs from "node:fs/promises";
+import throwErrorWhenFileExist from "../helpers/throwErrorWhenFileExist.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    const content = 'I am fresh and young';
+
+    try {
+        await throwErrorWhenFileExist(filePath);
+        await fs.writeFile(filePath, content);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await create();
