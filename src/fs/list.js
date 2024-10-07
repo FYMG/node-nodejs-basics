b38782a45ead04d2997b7be3f75e867ba6fba747
FYMG <svinpetr18@gmail.com>
2024-10-07 20:02:12 +0300

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import * as fs from "node:fs/promises";
+import throwErrorWhenFileNeverExist from "../helpers/throwErrorWhenFileNeverExist.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = path.join(__dirname, 'files');
+
+  try {
+    await throwErrorWhenFileNeverExist(dirPath)
+    const entries = await fs.readdir(dirPath, { withFileTypes: true })
+    console.log(entries);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await list();
