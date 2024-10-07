b377455c170de52b185ca4641d8a0c3fc54f12bf
FYMG <svinpetr18@gmail.com>
2024-10-07 21:46:22 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import * as os from "node:os";
+import {Worker} from "node:worker_threads";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const createWorker = (n) => {
+  const workerPath = path.join(__dirname, 'worker.js');
+
+  return new Promise(async (resolve, reject) => {
+    const worker = new Worker(workerPath, {
+      workerData: n
+    });
+
+    worker.on('message', async (message) => {
+      await worker.terminate();
+      resolve(message);
+    });
+
+    worker.on('error', async (error) => {
+      await worker.terminate();
+      reject(error);
+    });
+  });
+};
+
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = os.cpus().length;
+  const workers = [];
+
+  try {
+    for (let i = 0; i < numCores; i++) {
+      workers.push(createWorker(10 + i));
+    }
+
+    console.log(await Promise.all(workers))
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await performCalculations();
