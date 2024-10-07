08eba7b41b691978a385540d2d27a3ce6b8f227e
FYMG <svinpetr18@gmail.com>
2024-10-07 21:11:56 +0300

    feat: implement stream:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform } from 'node:stream';
+
+class ReverseTransform extends Transform {
+    _transform(chunk, _, callback) {
+        try {
+            const reversedChunk = chunk.toString().split('').reverse().join('') + '\n';
+            callback(null, reversedChunk);
+        } catch (err) {
+            callback(err);
+        }
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new ReverseTransform();
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+    reverseTransform.on('error', (err) => {
+        console.error(err);
+    });
 };
 
 await transform();
