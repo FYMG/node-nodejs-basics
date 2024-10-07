aceb5de41d5c82ef9984d5bbf06df1fd02b0f26c
FYMG <svinpetr18@gmail.com>
2024-10-07 21:02:45 +0300

    feat: implement stream:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import * as fs from "node:fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+  const stream = fs.createWriteStream(filePath, { encoding: 'utf8' });
+
+  process.stdin.pipe(stream);
+
+  stream.on('error', (err) => {
+    console.error('Error writing to file:', err);
+  });
 };
 
 await write();
