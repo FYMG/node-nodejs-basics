e86c1eee09b318b5e8ed29c4d908e6dc415d506c
FYMG <svinpetr18@gmail.com>
2024-10-07 20:06:44 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import throwErrorWhenFileNeverExist from "../helpers/throwErrorWhenFileNeverExist.js";
+import * as fs from "node:fs/promises";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        await throwErrorWhenFileNeverExist(filePath)
+        const content = await fs.readFile(filePath, 'utf-8')
+        console.log(content);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await read();
