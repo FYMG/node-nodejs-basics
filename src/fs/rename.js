8eda53b6fa4fd9dc3e3c292f4316441c4d3dd59b
FYMG <svinpetr18@gmail.com>
2024-10-07 20:11:34 +0300

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import throwErrorWhenFileNeverExist from "../helpers/throwErrorWhenFileNeverExist.js";
+import throwErrorWhenFileExist from "../helpers/throwErrorWhenFileExist.js";
+import * as fs from "node:fs/promises";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const rename = async () => {
-    // Write your code here 
+  const src = path.join(__dirname, 'files', 'wrongFilename.txt');
+  const dest = path.join(__dirname, 'files', 'properFilename.md');
+
+  try {
+    await Promise.all([
+      throwErrorWhenFileNeverExist(src),
+      throwErrorWhenFileExist(dest),
+    ])
+
+    await fs.rename(src, dest);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await rename();
