ee814988f76798ece692f78500bf9c32619dcc91
FYMG <svinpetr18@gmail.com>
2024-10-07 19:41:10 +0300

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
-const copy = async () => {
-    // Write your code here 
-};
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import throwErrorWhenFileExist from "../helpers/throwErrorWhenFileExist.js";
+import throwErrorWhenFileNeverExist from "../helpers/throwErrorWhenFileNeverExist.js";
+import * as fs from 'node:fs/promises';
 
-await copy();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const copy = async (src, dest) => {
+    try {
+        await Promise.all([
+                throwErrorWhenFileExist(dest),
+                throwErrorWhenFileNeverExist(src)
+            ]
+        )
+        const entries = await fs.readdir(src, { withFileTypes: true })
+
+        await fs.mkdir(dest, {recursive: true})
+
+        await Promise.all(
+            entries.map(async (entry) => {
+                const srcEntry = path.join(src, entry.name);
+                const destEntry = path.join(dest, entry.name);
+
+                if (entry.isDirectory()) {
+                    await copy(srcEntry, destEntry);
+                } else {
+                    await fs.copyFile(srcEntry, destEntry);
+                }
+            })
+        );
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+await copy(path.join(__dirname, 'files'), path.join(__dirname, 'files_copy'))
