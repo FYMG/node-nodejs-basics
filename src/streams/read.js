7a14318f5671db8df3bf740f13e96a2c0c82f77e
FYMG <svinpetr18@gmail.com>
2024-10-07 20:58:56 +0300

    feat: implement stream:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import * as fs from "node:fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const read = async () => {
-    // Write your code here 
-};
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  const stream = fs.createReadStream(filePath, {
+    encoding: 'utf-8',
+  });
+
+  stream.pipe(process.stdout);
+
+  stream.on('error', (err) => {
+    console.error(err);
+  });
+}
 
 await read();
